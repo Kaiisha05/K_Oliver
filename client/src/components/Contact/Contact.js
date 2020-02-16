21dbda7e6e3ff44c487642fa76e224dd9e39a959
Kaiisha05 <Kaiisha@yahoo.com>
2020-02-16 14:50:00 -0500

    contact page layout finalized

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import '../../components/Contact/style.css'
 function Contact() {
     return (
         <div className="wrap">
             <div className="main_content">
                 <div className="header">Let's Connect</div>
-                <div className="info">
+                {/* <div className="info"> */}
                     <div className="row row-cols-1 row-cols-md-3">
                         <div className="col mb-4">
-                        <div className="card h-100 text-center">
+                            <div className="card h-100 text-center">
                                 <a href='https://github.com/Kaiisha05' target="_blank"><i className="fab fa-github fa-7x"></i></a>
                                 <div className="card-body">
                                     <h3 className="card-title">Github</h3>
@@ -24,18 +24,23 @@ function Contact() {
                             </div>
                         </div>
                         <div className="col mb-4">
-                        <div className="card h-100 text-center">
+                            <div className="card h-100 text-center">
                                 <a href="#"><i className="fab fa-google-drive fa-7x"></i></a>
                                 <div className="card-body">
                                     <h3 className="card-title">Resume</h3>
                                 </div>
-                                </div>
                             </div>
                         </div>
                     </div>
+                    <div className="contact">
+                        <h3>Kaiisha Oliver</h3>
+                        <h3>kaiisha.tech@gmail.com</h3>
+                        <h3>(404)717-3229</h3>
+                    </div>
                 </div>
-            </div>
-   
+           </div>
+        // </div>
+
 
 
     )
