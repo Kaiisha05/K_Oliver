0b16d59677cb4047fa22d38dc1a437d6381c5b72
Kaiisha05 <Kaiisha@yahoo.com>
2020-02-16 17:17:14 -0500

    About me revised and styled

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../../components/About/style.css';
+import profile from '../../utils/images/ProfilePic.jpeg';
 
 function About() {
     return (
@@ -6,12 +8,14 @@ function About() {
             <div className="main_content">
                 <div className="header">About Me</div>
                 <div className="info">
-                    <p>I was born and raised in Savannah, Georgia. After graduating high school I moved to Atlanta, Ga to attend Georgia State University. I received my Bachelor's of Business Adminstration in Marketing with a Certificate in Hospitality with a concentration in Tradeshow and Event Planning.</p>
+                    <div><img src={profile} /></div>
+                    <p>I was born and raised in Savannah, Georgia. After graduating high school I moved to Atlanta, GA to attend Georgia State University. I received my Bachelor's of Business Adminstration in Marketing with a Certificate in Hospitality with a concentration in Tradeshow and Event Planning.</p>
                     <p>Upon graduation I began working for Schlesinger Associates, a Market Research facility. Within 4 years I achieved the role of Facility Manager before I made my exit. I went on to work for The Sherwin-Williams Company. After completing their Manager Trainee program I would go on to manage 3 differnt stores over the course of my 5 years. During this time, I received the award for Peek Performer within the NW Atlanta distrcit for 2 consecutive years and earned the Spectrum of Excellence President's Award for 3 consecutive years.</p>
-                    <p>Summer of 2019 I made the decision to part ways in hopes of finding my passion. Within a week of working my last day with S-W I was accepted into the Georgia Tech Professional Education Program for their 6-month Coding Bootcamp. Now that I am here, I am anxious to see me push my limits and gain new skills as I start my journey into a new career!</p>
+                    <p>I made the decision to part ways with S-W and begin my journey to become a Full Stack Web Developer. I recieved my Certificate from Georgia Tech in Atlanta, GA. I am anxious to push my limits, grow my new found skills and contribute in a meanigful way with a new company!</p>
                 </div>
             </div>
         </div>
+
     )
 };
 
