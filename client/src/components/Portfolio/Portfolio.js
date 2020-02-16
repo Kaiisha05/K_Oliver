698c9400f3c258787ee74d584248ffee3550f736
Kaiisha05 <Kaiisha@yahoo.com>
2020-02-16 18:22:20 -0500

    making sure heroku is up to date

--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -28,72 +28,57 @@ function Portfolio() {
                                         <Link to="https://github.com/Kaiisha05/Tis-the-Seasoning-v1" target="_blank">Github Repo</Link>
                                     </div>
                                 </div>
-                            </div>
-                      
+                            </div>                   
                         <div className="col mb-4">
                             <div className="card h-100">
                             <div className="apps"><img src={Friend}/></div>
-                                <div className="card-body">
-                                
+                                <div className="card-body">                              
                                     <h5 className="card-title">Friend Finder</h5>
-                                    <p className="card-text">A compatibility-based "FriendFinder" application will take in results from a Users' survey, then compare their answers with those from other Users. The most compatible "match" will be displayed.</p>
-                                 
+                                    <p className="card-text">A compatibility-based "FriendFinder" application will take in results from a Users' survey, then compare their answers with those from other Users. The most compatible "match" will be displayed.</p>             
                                         <Link to="https://friend-finder-app-ko.herokuapp.com/" target="_blank">View App</Link> {""}
                                         <Link to="https://github.com/Kaiisha05/Friend-Finder" target="_blank">Github Repo</Link>
                                     </div>
                                 </div>
-                            </div>
-                   
+                            </div>    
                         <div className="col mb-4">
                             <div className="card h-100">
                             <div className="apps"><img src={Trivia}/></div>
-                                <div className="card-body">
-                                 
+                                <div className="card-body">                        
                                     <h5 className="card-title">Trivia Game</h5>
-                                    <p className="card-text">A Disney-Inspired Trivia Game using JavaScript for the logic and jQuery to manipulate HTML.</p>
-                                 
+                                    <p className="card-text">A Disney-Inspired Trivia Game using JavaScript for the logic and jQuery to manipulate HTML.</p>                           
                                         <Link to="https://kaiisha05.github.io/Trivia-Game/" target="_blank">View App</Link> {""}
                                         <Link to="https://github.com/Kaiisha05/Trivia-Game" target="_blank">Github Repo</Link>
                                     </div>
                                 </div>
-                            </div>
-                    
+                            </div>               
                         <div className="col mb-4">
                             <div className="card h-100">
                             <div className="apps"><img src={Weather}/></div>
-                                <div className="card-body">
-                                 
+                                <div className="card-body">                            
                                     <h5 className="card-title text-center">Weather to Drink</h5>
-                                    <p className="card-text">Social App that allows User to get drink recipes based on their current or specified geographical location.</p>
-                                   
+                                    <p className="card-text">Social App that allows User to get drink recipes based on their current or specified geographical location.</p>                              
                                         <Link to="https://kaiisha05.github.io/Weather-to-Drink/" target="_blank">View App</Link> {""}
                                         <Link to="https://github.com/Kaiisha05/Weather-to-Drink" target="_blank">Github Repo</Link>
                                     </div>
                                 </div>
-                            </div>
-                  
+                            </div>              
                         <div className="col mb-4">
                             <div className="card h-100">
                             <div className="apps"><img src={Liri}/></div>
-                                <div className="card-body">
-                                  
+                                <div className="card-body">                             
                                     <h5 className="card-title">Liri-Node</h5>
-                                    <p className="card-text">LIRI - Language Interpretation Recognition Interface. Axios packages are used to retrieve data from: Bands in Town | Spotify | OMDB using the node commands within the terminal.</p>
-                                  
+                                    <p className="card-text">LIRI - Language Interpretation Recognition Interface. Axios packages are used to retrieve data from: Bands in Town | Spotify | OMDB using the node commands within the terminal.</p>                              
                                         <Link to="https://drive.google.com/file/d/1WelGH8861ej5zPrEUnIfXgdeHFrJrjeA/view" target="_blank">View Demo</Link> {""}
                                         <Link to="https://github.com/Kaiisha05/Liri-Node" target="_blank">Github Repo</Link>
                                     </div>
                                 </div>
                             </div>
-                     
                         <div className="col mb-4">
                             <div className="card h-100">
                             <div className="apps"><img src={Seasoning}/></div>
                                 <div className="card-body">
-                              
-                                    <h5 className="card-title">Tis the Seasoning-v2</h5>
+                              <h5 className="card-title">Tis the Seasoning-v2</h5>
                                     <p className="card-text">MERN Recipe Sharing App</p>
-                              
                                         <Link to="https://tranquil-ocean-59356.herokuapp.com/" target="_blank">View App</Link> {""}
                                         <Link to="https://github.com/Kaiisha05/Tis-the-Seasoning-v2-MERN" target="_blank">Github Repo</Link>
                                     </div>
