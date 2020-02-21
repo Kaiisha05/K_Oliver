043b59e04986958aba825c298065b001e24c2c21
Kaiisha05 <Kaiisha@yahoo.com>
2020-02-21 11:43:41 -0600

    update header component to render on Contact component. missed in the previous update

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,18 +2,15 @@ import React, { Component } from 'react';
 import './Header.css';
 
 
-
 class Header extends Component {
     state = {};
     render() {
-      return (
-        <div className="header">
-         
-            {this.props.title}
-          
-        </div>
-      );
+        return (
+            <div className="header">
+                {this.props.title}
+            </div>
+        );
     }
-  }
-  
-  export default Header;
+}
+
+export default Header;
